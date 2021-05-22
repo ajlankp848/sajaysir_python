d970d9e86cab631aacf389077ee4b77851e2c12c
ajlankp848 <ajlantkm848@gmail.com>
2021-05-22 10:42:46 +0530

    sqlqueries

---
+++ mysqlBasics/mysqlBasics.py
@@ -0,0 +1,35 @@
+#what is data base?
+#data base is a collection of tables
+
+
+#banking projrct
+#1.create a database
+#data base is a collection of tables, which have rows and coloumns
+#id,name,desig,location,salary
+#each row is called records
+
+
+# database queries
+#create database databsename;
+#listing all databases-- show databases;
+#use databasename;--query for using a specific databse
+
+#querys for creating table
+#create table tablename(coloumnnamedatatype,coloumnnamedatatype,.......);
+#ex-: create table employee(eid varchar(23),ename varchar(25),desig varchar(25),salary int,experience int,location varchar(25)
+#show tables- to show table
+
+#to know fields in table or listing structure of table
+#desc table_name;
+
+#insert records into table
+#insert into table_name(coloumn1,coloumn2,....)values(value1,value2,...);
+
+#select * from table_name;
+#select ename from employee;
+#select * from employee where salary > 30000;
+#select * from employee where location = ekm;
+#select * from employee where salary > 20000 and experience>1;
+
+
+
